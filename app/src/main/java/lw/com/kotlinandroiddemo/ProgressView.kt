da24c3380b0b8374f7047c627e1f17426689fac3
卢威 <wei_lu1@kingdee.com>
2017-12-14 17:55:00 +0800

    自定义view

--- app/src/main/java/lw/com/kotlinandroiddemo/ProgressView.kt
+++ app/src/main/java/lw/com/kotlinandroiddemo/ProgressView.kt
@@ -3,18 +3,32 @@ package lw.com.kotlinandroiddemo
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.util.AttributeSet
 import android.view.View
 
 /**
  * Created by engle on 2017/12/14.
  */
-class ProgressView(context: Context) : View(context) {
+class ProgressView : View {
     var progress: Int = 0
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        var paint: Paint = Paint()
+        var paint = Paint()
+        paint.strokeWidth = 10f
+        paint.strokeCap = Paint.Cap.BUTT
+
+
+//        canvas?.drawArc(RectF(100f, 100f, 500f, 500f),
+//                180f, 90f, false, paint)
+
+        canvas?.drawCircle(200f, 200f, 100f, paint)
+
 
     }
 }
